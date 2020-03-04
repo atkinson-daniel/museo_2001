f4dd2e1f21d635e3898a5ef68aac1310ffa12b44
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-03-04 09:59:32 -0700

    Add photographs_by_artist, artists_with_multiple_photographs, photographs_taken_by_artist_from methods to Curator class

--- lib/curator.rb
+++ lib/curator.rb
@@ -15,6 +15,35 @@ class Curator
   end
 
   def find_artist_by_id(id)
-    @artists.find {|artist| artist.id == "1"}
+    @artists.find {|artist| artist.id == id}
+  end
+
+  def photographs_by_artist
+    artists_collection = @artists.reduce({}) do |accum, artist|
+      accum[artist] = []
+      accum
+    end
+
+    @photographs.each do |photograph|
+      artist = find_artist_by_id(photograph.artist_id)
+      artists_collection[artist] << photograph
+    end
+    artists_collection
+  end
+
+  def artists_with_multiple_photographs
+    found = photographs_by_artist.select do |artist, photographs|
+      photographs.length > 1
+    end
+
+    found.map {|artist, photographs| artist.name}
+  end
+
+  def photographs_taken_by_artist_from(country)
+    found = photographs_by_artist.select do |artist, photographs|
+      artist.country == country
+    end
+
+    found.flat_map {|artist, photographs| photographs}
   end
 end
